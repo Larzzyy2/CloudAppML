ab9db62a624a91c83b9e129bbd99cd6f5f0476b0
Lars <larssuffys@hotmail.com>
2016-11-01 15:33:19 +0100

    Name field can be toggle disabled or editable Added Session Package

--- Meteor/Presentation/Presentation/imports/ui/mypresentations.js
+++ Meteor/Presentation/Presentation/imports/ui/mypresentations.js
@@ -1,7 +1,9 @@
 import './myPresentations.html';
+import { Session } from 'meteor/session'
 
 //Subscribe to presentations owned by the user
 Meteor.subscribe('myPresentations');
+Session.set('toggleDisabled', disabled="disabled");
 
 //Helper for getting all Presentations
 Template.myPresentations.helpers({
@@ -11,13 +13,28 @@ Template.myPresentations.helpers({
         var data = Presentations.find({createdBy: currentUserId});
         return data;
     },
+    toggleDisabled: function(){
+        return Session.get('toggleDisabled');
+    }
+    
 });
 Template.myPresentations.events({
    "click #delete" (event){
+       //Removes the selected presentation from collection
        Presentations.remove(this._id);
        
    },
     "click #edit" (event){
+        if(Session.equals("toggleDisabled", disabled="disabled"))
+            {
+                Session.set('toggleDisabled', '');
+            }
+        else
+            {
+                Session.set('toggleDisabled', disabled="disabled")
+            }
         //Code for editing the presentation name
+        //var updatedName = //TODO
+        //Presentations.update({_id: this._id}, {$set: {name: updatedName}})
     }
 });
