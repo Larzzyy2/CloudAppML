cabb7ceae02f5d73f231250a2b3324d62d1bcff9
Lars <larssuffys@hotmail.com>
2016-12-11 12:22:52 +0100

    View for adding questions updated, questions can be pushed to db

--- Meteor/imports/templates/Detail/detail.js
+++ Meteor/imports/templates/Detail/detail.js
@@ -1,12 +1,26 @@
 import './detail.html';
-Session.setDefault('AnswerCounter',1);
-Session.setDefault('Open',false);
-Session.setDefault('MultipleChoice',true);
+import './addQuestion.js'
 
 Meteor.subscribe('Types');
 Meteor.subscribe('Questions')
+Session.setDefault('newQuestion',false);
 
-                           
+Template.DetailLayout.events({
+   "click #back" (){
+    FlowRouter.go('/');
+    Session.set('newQuestion', false);
+   },
+    'click #createQuestion'(){
+        Session.set('newQuestion', true);
+        //Code to insert empty data
+   },
+});
+
+Template.DetailLayout.helpers({
+    newQuestion(){
+        return Session.get('newQuestion');
+    },
+})                   
 Template.Info.helpers({
         presentation(){
         //Gets all presentations associated with user
@@ -14,66 +28,28 @@ Template.Info.helpers({
         var data = Presentations.find({_id: ID});
         return data;
     },
-})
-
-Template.DetailLayout.events({
-   "click #back" (){
-    FlowRouter.go('/');
-   },
 });
 
-Template.Answers.events({
-    "click #AddAnswer"(){
-        //To DO extra answer
-    },
-
+Template.QuestionOverview.helpers({
+    Questions(){
+        var ID = Session.get('currentPresentationID');
+        var data = Questions.find({PresentationID: ID});
+        return data;
+    }
 });
 
-Template.Question.events({
-        "submit #AddQuestion"(e){
-         e.preventDefault();
-        //Get value from form element
-        const target = e.target;
-        const Question = target.QuestionString.value;
-        var currentPresentationID = Session.get('currentPresentationID');
-        var QuestionID;
-            if(Session.get('MultipleChoice')){
-                    var data = Types.findOne({name:"Multiple Choice"});
-                    QuestionID = data._id;
-                }
-           else
-                {
-                    var data  = Types.findOne({name: "Open"});
-                   QuestionID = data._id;
-                }
-
-        Questions.insert({
-            QuestionString: Question,
-            TypeID: QuestionID,
-            PresentationID: currentPresentationID
-        });
-        
-        target.QuestionString.value="";
-            
-    },
-});
-
-Template.QuestionTypes.events({
-        "click #MultipleChoice" (){
-        Session.set('Open', false);
-        Session.set('MultipleChoice', true);
+Template.QuestionOverview.events({
+    "click #edit"(){
         
     },
-    "click #Open"(){
-        Session.set('MultipleChoice', false);
-        Session.set('Open', true);
+    "click #delete"(){
+        Questions.remove(this._id);
     },
 });
+
 Template.Answers.helpers({
-        MultipleChoice(){
-        return Session.get('MultipleChoice');
-    },
-        AnswerCounter(){
-        return Session.get('AnswerCounter');
-    },
-})
+    answeroptions(){
+        var currentPresentationID = Session.get('currentPresentationID');
+        var data = AnswerOptions.find({QuestionID: ""});
+    }
+});
